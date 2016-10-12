d4a06cceba93250ecd5043d419407577762c64f2
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2016-10-12 13:16:05 +0200

    Version in URL is now optional
    Signed-off-by: Petr Stehlik <xstehl14@stud.fit.vutbr.cz>

--- api/module.py
+++ api/module.py
@@ -2,8 +2,11 @@ from flask import Blueprint
 from api import config
 
 class Module(Blueprint):
-	def __init__(self, name, import_name, url_prefix=None):
-		self.url_prefix = '/' + config['api']['version'] + url_prefix
+	def __init__(self, name, import_name, url_prefix=None, no_version=False):
+		if not no_version:
+			self.url_prefix = '/' + config['api']['version'] + url_prefix
+		else:
+			self.url_prefix = url_prefix
 		super().__init__(name, import_name, url_prefix = self.url_prefix)
 
 	def prefix(p):
