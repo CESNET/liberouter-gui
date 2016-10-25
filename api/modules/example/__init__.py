582257afb64340c84c684957ba5133a62a18f39a
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2016-10-25 17:03:26 +0200

    Example module protected endpoint
    Signed-off-by: Petr Stehlik <xstehl14@stud.fit.vutbr.cz>

--- api/modules/example/__init__.py
+++ api/modules/example/__init__.py
@@ -7,3 +7,4 @@ module_bp = Module('module', __name__, url_prefix = '/module')
 from api.modules.example.base import *
 
 module_bp.add_url_rule('/', view_func = hello_world, methods=['GET'])
+module_bp.add_url_rule('/protected', view_func = protected_hello, methods=['GET'])
--- api/modules/example/base.py
+++ api/modules/example/base.py
@@ -1,2 +1,8 @@
+from api import auth
+
 def hello_world():
 	return("Hello from the module!")
+
+@auth.required()
+def protected_hello():
+	return("Protected hello")
