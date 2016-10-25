c2ad5671050aa37dc2d6ce2ef85f5ac98f190608
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2016-10-25 16:59:34 +0200

    User: methods for de/serializing of the user

--- api/user.py
+++ api/user.py
@@ -1,39 +1,61 @@
+from .error import ApiException
 from .role import Role
 
-class UserException(Exception):
+class UserException(ApiException):
 	status_code = 401
 
-	def __init__(self, message, status_code=None, payload=None):
-		Exception.__init__(self)
-		self.message = message
-		if status_code is not None:
-			self.status_code=status_code
-		self.payload = payload
-
-	def to_dict(self):
-		rv = dict(self.payload or ())
-		rv['message'] = self.message
-		return rv
-
 class User(object):
-	role = Role.guest
-	config = {}
+	role = Role.undefined
+	settings = {}
 
 	def __init__(self,
 			username,
 			user_id=None,
-			config=None,
+			settings=None,
 			role=None,
 			password="",
 			email=""):
 		self.username = username
 		self.user_id = user_id
 
-		if config is not None:
-			self.config = config
+		if settings is not None:
+			self.settings = settings
 
 		if role is not None:
 			self.role = role
 
 		self.password = password
 		self.email = email
+
+	def setRole(self, role):
+		if role == "admin" or role == Role.admin:
+			self.role = Role.admin
+		elif role == "user" or role == Role.user:
+			self.role = Role.user
+		elif role == "guest" or role == Role.guest:
+			self.role = Role.guest
+
+	def to_dict(self):
+		"""
+		Return the internal data in dictionary
+		"""
+		return({
+			"username" : self.username,
+			"user_id" : self.user_id,
+			"settings" : self.settings,
+			"role" : int(self.role),
+			"password" : self.password,
+			"email" : self.email,
+		})
+
+	@classmethod
+	def from_dict(self, user):
+		"""
+		Create new user from dictionary
+		"""
+		return(self(user.get("username", "undefined"),
+			role = user.get("role", Role.undefined),
+			settings = user.get("settings", {}),
+			user_id = str(user["_id"]),
+			password = user.get("password", ""),
+			email = user.get("email", "")))
