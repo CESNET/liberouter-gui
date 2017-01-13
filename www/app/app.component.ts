52b61efb98eaeeb47e540f2a1f28e9e88802933e
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2017-01-13 13:58:43 +0100

    Update basic module view not to render useless stuff

--- www/app/app.component.ts
+++ www/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { LoginBox } from './components/login.component';
 
 @Component({
-  selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1><login-box></login-box>`,
+	selector: 'my-app',
+	template: `<router-outlet></router-outlet>`
 })
 export class AppComponent  { name = 'Angular'; }
