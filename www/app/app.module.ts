2a2a4f95a91b64da43c76a83bae3b6081b3303e9
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2017-01-13 14:01:22 +0100

    Implement basic authentication and login form

--- www/app/app.module.ts
+++ www/app/app.module.ts
@@ -1,12 +1,54 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule }			from '@angular/core';
+import { RouterModule }		from '@angular/router';
+import { Routes }			from '@angular/router';
+import { BrowserModule }	from '@angular/platform-browser';
+import { FormsModule }		from '@angular/forms';
+import { MaterialModule }   from "@angular/material";
+import { FlexLayoutModule } from "@angular/flex-layout";
 
 import { AppComponent }  from './app.component';
 import { LoginBox }  from './components/login.component';
+import { UserToolbar }  from './components/toolbar.component';
+import { Home }  from './components/home.component';
+import { NullComponent }  from './components/null.component';
+
+const appRoutes: Routes = [
+	{
+		path : 'login',
+		component : LoginBox
+	},
+	{
+		path: '',
+		component: Home
+		},
+	{
+		path: 'heroes',
+		component: NullComponent
+	},
+
+	{
+		path: '**',
+		component: NullComponent
+	}
+];
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent, LoginBox ],
-  bootstrap:    [ AppComponent, LoginBox ]
+	imports : [
+		BrowserModule,
+		FormsModule,
+		MaterialModule.forRoot(),
+		FlexLayoutModule.forRoot(),
+		RouterModule.forRoot(appRoutes)
+	],
+	declarations : [
+		AppComponent,
+		LoginBox,
+		NullComponent,
+		UserToolbar,
+		Home
+	],
+	bootstrap : [
+		AppComponent
+	]
 })
 export class AppModule { }
--- www/app/components/login.component.ts
+++ www/app/components/login.component.ts
@@ -1,7 +1,77 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from './user.component';
+import { AuthService } from '../services/index';
 
 @Component({
   selector: 'login-box',
   templateUrl: 'app/components/login.html',
+  providers : [AuthService]
 })
-export class LoginBox  { }
+export class LoginBox  {
+	loading = false;
+	user : User = {
+		id : "",
+		username : "",
+		password : "",
+		email : ""
+	};
+	formError = false;
+	formErrorMsg = "";
+
+	constructor(
+		private router : Router,
+		private authService: AuthService) {}
+
+	ngOnit() {
+	// TODO: check if the user is logged in and if so redirect them to HP
+	}
+
+	setError(msg : string) {
+		this.formError = true;
+		this.formErrorMsg = msg;
+		this.loading = false;
+	}
+
+	unsetError() {
+		this.formError = false;
+		this.formErrorMsg = "";
+		this.loading = false;
+	}
+
+	login() {
+		// Authenticate the user and redirect them
+		this.loading = true;
+
+		if (this.user.username == "" || this.user.password == "") {
+			this.setError("Missing username or password");
+			return;
+		}
+
+		this.authService.login(this.user.username, this.user.password)
+			.subscribe(
+				data => {
+					console.log("Success!!!");
+					this.unsetError();
+					this.router.navigate(['/success']);
+				},
+				error => {
+					let body = JSON.parse(error['_body']);
+					this.setError(body['message']);
+				}
+			);
+	}
+
+	logout() {
+		this.authService.logout()
+			.subscribe(
+				data => {
+					console.log('Success logging out.');
+				},
+				error => {
+					console.log('Error logging out.');
+				}
+			);
+	}
+
+}
