8ce4d5ca72a1b5746fb992490d187b737693d838
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2017-01-25 13:39:22 +0100

    AuthGuard now supports roles

--- www/app/guards/auth.guard.ts
+++ www/app/guards/auth.guard.ts
@@ -1,19 +1,54 @@
 import { Injectable } from '@angular/core';
-import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { CanActivate, Router,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  CanActivateChild,
+  NavigationExtras,
+  CanLoad, Route } from '@angular/router';
 
 @Injectable()
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanLoad {
 
-    constructor(private router: Router) { }
+	constructor(private router: Router) { }
 
-    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-        if (localStorage.getItem('currentUser')) {
+	isLoggedIn() : boolean {
+	    if (localStorage.getItem('currentUser')) {
             // logged in so return true
             return true;
         }
 
         // not logged in so redirect to login page with the return url
+        return false;
+	}
+
+	canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+		console.debug('Hello Guard');
+        if (this.isLoggedIn()) {
+		// logged in so return true
+			console.log(route.data)
+			let user = JSON.parse(localStorage.getItem('currentUser'));
+			if (!route.data['role']) {
+				console.warn('No role is set for route \'' + route.data['path'] + '\'');
+				return true;
+			}
+
+			if (user.user.role <= route.data['role']) {
+				return true;
+			}
+
+			console.warn('User is not allowed to access \'' + route.data['path'] + '\'')
+			return false;
+        }
+
+        // not logged in so redirect to login page with the return url
         this.router.navigate(['/login'], { queryParams: { returnUrl: state.url }});
         return false;
-    }
+	}
+
+	canLoad(route : Route) : boolean {
+		if (this.isLoggedIn()) {
+			return true;
+		}
+		return false;
+	}
 }
