d3bc9312ef7d79e1e1dbbfb5a01e9ebc1a275de3
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2017-03-17 11:36:53 +0100

    GUI: Nemea: add link to FTAS

--- www/src/app/modules/nemea/events/event-detail/event-detail.component.ts
+++ www/src/app/modules/nemea/events/event-detail/event-detail.component.ts
@@ -7,10 +7,31 @@ import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./event-detail.component.scss']
 })
 export class EventDetailComponent implements OnInit {
-  @Input() event : Object;
+  @Input() data : Object;
+
+  ftasUrl : string = "/ftas";
+  ftasUrlParams : Object = {};
 
   constructor(public activeModal: NgbActiveModal) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.generateFtasUrl();
+  }
+
+  generateFtasUrl() {
+    console.log(this.data);
+
+    if (this.data["Source"] != undefined && this.data["Source"][0]["IP4"] != undefined) {
+        this.ftasUrlParams["src_ip"] = this.data["Source"][0]["IP4"][0];
+        console.log("adding src IP")
+    }
+
+    if (this.data["Target"] != undefined && this.data["Target"][0]["IP4"] != undefined) {
+        this.ftasUrlParams["dst_ip"] = this.data["Target"][0]["IP4"][0];
+        console.log("adding dst IP")
+    }
+
+    console.log(this.ftasUrl)
+  }
 
 }
