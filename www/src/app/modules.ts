80f3a734cf7b159c95a424332a2317934eb1e086
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2017-04-06 11:52:43 +0200

    New module SecurityCloud
    Creates an iframe to SecurityCloud GUI instance with optional
    setting of looking up by time of an event (clickable through NEMEA Events)

--- www/src/app/modules.ts
+++ www/src/app/modules.ts
@@ -2,10 +2,12 @@
 import { usersModule } from './modules/users/users.module';
 import { nemeaModule } from './modules/nemea/nemea.module';
 import { FtasModule } from './modules/ftas/ftas.module';
+import { SecurityCloudModule } from './modules/security-cloud/sc.module';
 
 export const modules : Array<Object> = [
 	//		dummyModule,
 	nemeaModule,
 	FtasModule,
+	SecurityCloudModule,
 	usersModule
 ]
--- www/src/environments/environment.ts
+++ www/src/environments/environment.ts
@@ -23,5 +23,23 @@ export const environment = {
       * Can be a list (as string): "1,2,5,10"
       */
     output : "1402"
+  },
+  securityCloud : {
+    /**
+      * simple URL (without protocol) to FTAS instance
+      */
+    url : undefined,
+    /**
+      * Full URL to filtering script (usually "example.com/ftas/stat.pl")
+      * Specify URL without URL (https is forced)
+      * If this option is not set, url must be.
+      * If both are set, fullUrl is used.
+      */
+    fullUrl : "/scgui",
+    /**
+      * Set resolution of the graph output
+      * Default: 2
+      */
+    resolution : 2
   }
 };
