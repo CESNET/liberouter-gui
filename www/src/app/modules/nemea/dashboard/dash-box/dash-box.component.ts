c221bc7aa333a409bd50d6a8eee4b3b2990028e5
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2017-06-06 15:13:45 +0200

    NEMEA: Dashboard box working piechart

--- www/src/app/modules/nemea/dashboard/dash-box/dash-box.component.ts
+++ www/src/app/modules/nemea/dashboard/dash-box/dash-box.component.ts
@@ -1,9 +1,24 @@
-import { Component, OnInit, Input, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, OnInit, Input, Output, ViewChild, EventEmitter } from '@angular/core';
+import { PieChartComponent } from '@swimlane/ngx-charts';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { DashBoxModalComponent } from '../dash-box-modal/dash-box-modal.component';
+import { Box } from '../dashboard.component';
+
 
 import { BoxService } from '../box.service';
-import * as nv from 'nvd3';
 
-
+/**
+  * Dashboard Box component
+  *
+  * It encapsulates a box which can of several types:
+  *		- piechart
+  *		- barchart
+  *		- sum
+  *		- top
+  *
+  * For displaying charts ngx-charts library is used. Before you say I should
+  * use nvd3 it is very buggy in Angular 4 (ng2-nvd3 and angular-nvd3).
+  */
 @Component({
   selector: 'dash-box',
   templateUrl: './dash-box.component.html',
@@ -13,32 +28,43 @@ import * as nv from 'nvd3';
 export class DashBoxComponent {
 
 	@Input() box;
-	@ViewChild('chart') chart;
 
-	private data = [ { "key": [ "Hello" ], "x:": 10 }, { "key": [ "Hello2" ], "x:": 20 } ];
+	// Keeps the original model
+	private backup;
 
-	constructor(private boxService : BoxService) {
-		window.onresize = null;
+	@Input() index;
+
+	/**
+	  * We use event emitter for parent interaction which holds the boxes array
+	  *
+	  * It might look that this should be in the dashboard component but
+	  * this component really operates only on itself.
+	  */
+	@Output() onDelete = new EventEmitter<Object>();
+	@Output() onDuplicate = new EventEmitter<Object>();
+	@Output() onSave = new EventEmitter<Object>();
+
+	@ViewChild(PieChartComponent)
+	private piechart;
+
+	private data = [];
+	private modalRef : any;
+
+	constructor(private boxService : BoxService,
+			   private modalService: NgbModal) {
 	}
 
+	/**
+	  * Shift time and fetch data on init
+	  */
 	ngOnInit() {
-		delete this.box.data;
-//		console.log(this.box.data);
+		// Shift time for fetching correct data
 		this.timeShift();
-		console.log("should get data from", this.box.beginTime)
 
+		// Fetch data
 		this.boxService.aggregate(this.box).subscribe(
 			(data) => {
-				console.debug("Getting data");
-				//this.data = data;
-				//this.data = [{"key" : ["Hello"], "x:" : 10}]
-				console.log(data);
-				//setTimeout(_ => { this.data = data }, 100);
-				//console.log(data);
-				//console.log(this.chart["ngNvD3"])
-				//this.chart["ngNvD3"].clearElement();
-				//this.chart["ngNvD3"].isViewInitialize(false);
-				//this.chart["ngNvD3"]["chart"].update()//WithData(this.box.data, this.box.options);
+				this.data = data;
 			},
 			(err) => {
 				console.error(err);
@@ -59,5 +85,87 @@ export class DashBoxComponent {
         this.box.endTime = Math.floor(Number(now)/1000);
     }
 
+	/**
+	  * Update the chart
+	  * Calls child method update()
+	  * More info here: https://github.com/swimlane/ngx-charts/blob/master/src/pie-chart/pie-chart.component.ts
+	  */
+    update() {
+    	if (this.piechart != undefined)
+			this.piechart.update();
+    }
+
+	/**
+	  * Trigger a modal window which can edit the given box
+	  */
+	edit() {
+		// Create a backup of current model
+		this.backup = Object.assign({}, this.box);
+
+		// Open the modal and check results
+		this.modalRef = this.modalService.open(DashBoxModalComponent)
+		this.modalRef.componentInstance.data = this.box;
+		this.modalRef.result.then(
+			(result) => {
+				/**
+				  * Closed with a button, replace current model with the new one
+				  * Also would be nice to reload data
+				  */
+
+				// Shift the time
+				this.timeShift();
+				this.save();
+
+				this.boxService.aggregate(this.box).subscribe(
+					(data) => {
+						this.data = data;
+					},
+					(err) => {
+						console.error(err);
+					}
+				);
+
+			},
+			(reason) => {
+				// Dismissal of the modal, revert backup
+				console.debug("Modal dismissed");
+				this.box = Object.assign({}, this.backup);
+			}
+		);
+	}
+
+	/**
+	  * Remove this box from the array of boxes
+	  */
+	remove() {
+		console.debug("Deleting box");
+		this.onDelete.emit({"box" : this.box, "index" : this.index });
+	}
+
+	/**
+	  * Duplicate this box in the boxes array
+	  */
+	duplicate() {
+		this.onDuplicate.emit(this.box);
+	}
+
+	save() {
+		this.onSave.emit({});
+	}
+
+	resize(event) {
+		console.log(event);
+		this.update();
+	}
+
+	/**
+	  * Called when onDragStop is emitted to save the change
+	  */
+	itemChange(event) {
+		this.update();
+
+		// Must wait with saving, the change is not propagated fash enough
+		setTimeout(_ => { this.save() }, 100);
+	}
 
 }
