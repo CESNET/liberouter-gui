3ba057c666f95332f5264074df4548fce0a1536d
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2017-06-07 16:51:41 +0200

    NEMEA: support for count and top boxes

--- www/src/app/modules/nemea/dashboard/box.service.ts
+++ www/src/app/modules/nemea/dashboard/box.service.ts
@@ -45,6 +45,44 @@ export class BoxService {
 			.catch(this.handleError);
 	}
 
+	count(box) {
+		let requestOptions = new RequestOptions();
+		let params: URLSearchParams = new URLSearchParams();
+
+		params.set('begintime', box['beginTime']);
+		params.set('endtime', box['endTime']);
+		params.set('category', "any");
+
+		requestOptions.search = params;
+
+		return this.http.get('/nemea/events/count', requestOptions).map(
+			(response : Response) => {
+				console.log("response", response)
+				let body = response.json()
+				return body;
+			})
+			.catch(this.handleError);
+	}
+
+	top(box) {
+		let requestOptions = new RequestOptions();
+		let params: URLSearchParams = new URLSearchParams();
+
+		params.set('begintime', box['beginTime']);
+		params.set('endtime', box['endTime']);
+
+		requestOptions.search = params;
+
+		return this.http.get('/nemea/events/top', requestOptions).map(
+			(response : Response) => {
+				console.log("response", response)
+				let body = response.json()
+				return body;
+			})
+			.catch(this.handleError);
+	}
+
+
 	private handleError(err : Response | any) {
 		return Promise.reject(err);
 	}
