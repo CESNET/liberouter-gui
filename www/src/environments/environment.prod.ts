3bec37b33d67bb90dce561460c6973cc54e3b151
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2017-06-19 11:09:53 +0200

    FTAS: remove ftas from environment

--- www/src/environments/environment.prod.ts
+++ www/src/environments/environment.prod.ts
@@ -8,24 +8,6 @@ export const environment = {
   	* Used only when fetching config.json failed
   	*/
   apiUrl : "/libapi",
-  ftas : {
-    /**
-      * simple URL (without protocol) to FTAS instance
-      */
-    url : "ftas.cesnet.cz",
-    /**
-      * Full URL to filtering script (usually "example.com/ftas/stat.pl")
-      * Specify URL without URL (https is forced)
-      * If this option is not set, url must be.
-      * If both are set, fullUrl is used.
-      */
-    fullUrl : undefined,
-    /**
-      * Specify which output machines will be used
-      * Can be a list (as string): "1,2,5,10"
-      */
-    output : "1402"
-  },
   securityCloud : {
     /**
       * simple URL (without protocol) to FTAS instance
