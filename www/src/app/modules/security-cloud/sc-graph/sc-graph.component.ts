1c19c6f89969fe4855d0679eb3fa2b8cdd51dc5a
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2017-06-22 15:14:25 +0200

    SC: fix import and indentation

--- www/src/app/modules/security-cloud/sc-graph/sc-graph.component.ts
+++ www/src/app/modules/security-cloud/sc-graph/sc-graph.component.ts
@@ -3,7 +3,7 @@ import { Component, Input, Output, OnInit, EventEmitter } from '@angular/core';
 import { SimpleChanges } from '@angular/core';
 
 // Local modules
-import { ProfileMap, Channel } from '../sc.Profile';
+import { ProfileMap, Channel } from '../modules/Profile';
 import { TimeSpecs, TimeSelection, TimeView } from '../modules/TimeSpecs';
 import { RRDVariables } from '../modules/RRDVariables';
 
@@ -21,13 +21,13 @@ export class ScGraphComponent implements OnInit {
 	/* EXTERNAL VARIABLES */
 	@Input() profiles: ProfileMap;
 	@Input() selectedProfile : string;
-	
+
 	/* 2-WAY DATA BINDING */
 	@Input() time : TimeSpecs;
 	@Output() timeChange = new EventEmitter(); // this.timeChange.emit(this.time)
 	@Input() timeUpdated : boolean;
 	@Output() timeUpdatedChange = new EventEmitter();
-	
+
 	/* INTERNAL VARIABLES */
 	selectedVar : number = 0; ///< Index to RRDVariables
 	channels : ChannelSettings[]; ///< ngModel for channel checkboxes
@@ -36,18 +36,18 @@ export class ScGraphComponent implements OnInit {
 
 	// NOTE: Cannot make const variable within class
 	// NOTE: Cannot move it outside, template won't recognize it
-	
+
 	renderSettings : string = "stacked";
-	
+
 	constructor() { }
 
 	ngOnInit() { }
-	
+
 	/**
 	 *  @brief Handle external changes to @Input based variables
-	 *  
+	 *
 	 *  @param [in] changes Dictionary with changed variables
-	 *  
+	 *
 	 *  @details If selectedProfile was changed, then ChannelSettings list must
 	 *  be rebuilt. If selected times were changed, then textbox for printing
 	 *  them has to be updated. FrameTime changes are handled by graph-render.
@@ -62,7 +62,7 @@ export class ScGraphComponent implements OnInit {
 			}
 		}
 	}
-	
+
 	/**
 	 *  @brief Create control array for filtering channels displayed in graph
 	 *
@@ -73,17 +73,17 @@ export class ScGraphComponent implements OnInit {
 	buildChannels() {
 		// Get list of channels of a current profile
 		let channels : Channel[] = this.profiles.getProfile(this.selectedProfile).channels;
-		
+
 		// Forget any previous content, create new array
 		this.channels = new Array<ChannelSettings>();
 		for (let channel of channels) {
 			this.channels.push({name: channel.name, checked: true});
 		}
 	}
-	
+
 	/**
 	 *  @brief Method for ensuring 2-way data binding across the component tree
-	 *  
+	 *
 	 *  @details This method emits events for variables time and timeUpdated
 	 */
 	triggerEmitters() {
@@ -91,30 +91,30 @@ export class ScGraphComponent implements OnInit {
 		this.timeUpdated = !this.timeUpdated;
 		this.timeUpdatedChange.emit(this.timeUpdated);
 	}
-	
+
 	/**
 	 *  @brief Displays new selected times in a dedicated textbox
-	 *  
+	 *
 	 *  @details This function only updates value of this.readableTime which is
 	 *  bind to #SelectedTime.
-	 *  
+	 *
 	 *  @todo Convert time.sel.bgn and time.sel.end from JS timestamps to readable
 	 *  dates.
 	 */
 	updateSelectedTimeViewbox() {
 		this.readableTime = String(this.time.sel.bgn);
-		
+
 		if (this.time.sel.bgn != this.time.sel.end) {
 			this.readableTime += " - " + String(this.time.sel.end);
 		}
 	}
-	
+
 	/**
 	 *  @brief Handle for updating selected times based on datepicker
-	 *  
+	 *
 	 *  @param [in] event Object with attributes year, month, day. There specify
 	 *  new selected timeslot
-	 *  
+	 *
 	 *  @details View frame must be aligned in a way that selected interval is
 	 *  in the middle.
 	 */
@@ -122,16 +122,16 @@ export class ScGraphComponent implements OnInit {
 		// Selecting only single timeslot
 		this.time.sel.bgn = new Date(event.year, event.month, event.day).getTime();
 		this.time.sel.end = this.time.sel.bgn;
-		
+
 		// Cursor will be in center of the view
 		this.time.view.bgn = this.time.sel.bgn - this.time.view.res / 2;
 		this.time.view.end = this.time.sel.bgn + this.time.view.res / 2;
-		
+
 		// Since ngOnChanges does not catch update from the inside
 		this.updateSelectedTimeViewbox();
 		this.triggerEmitters();
 	}
-	
+
 	getRRDConstant() {
 		return RRDVariables[this.selectedVar];
 	}
