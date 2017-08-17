a11df2e6fec772e5a895d502cb956c83f0058275
nerudaj <xnerud01@stud.fit.vutbr.cz>
2017-08-17 13:47:01 +0200

    WSGI: Sys path insert position
    It is recommended to insert at second position of sys.path

--- api/wsgi.py
+++ api/wsgi.py
@@ -1,6 +1,6 @@
 import sys
 import os
-sys.path.insert(0, os.path.dirname(__file__))
+sys.path.insert(1, os.path.dirname(__file__))
 
 activate_this = os.path.join(os.path.dirname(__file__), 'venv/bin/activate_this.py')
 with open(activate_this) as file_:
