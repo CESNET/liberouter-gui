c3e0864658fac56a4592847d7fed99decf2b333f
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2017-09-21 11:26:59 +0200

    # This is a combination of 6 commits. # This is the 1st commit message: TCI: Add Travis-CI config file
    # The commit message #2 will be skipped:
    
    #	TCI: add build script for BE and FE
    
    # The commit message #3 will be skipped:
    
    #	TCI: link to travis-build.sh in Travis config
    
    # The commit message #4 will be skipped:
    
    #	TCI: add pip dep for bootstrap
    
    # The commit message #5 will be skipped:
    
    #	TCI: add sudo
    
    # The commit message #6 will be skipped:
    
    #	TCI: add python3 things

---
+++ travis-build.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Frontend build procedure
+cd $TRAVIS_BUILD_DIR/frontend
+npm install
+ng build --prod --bh="/" --aot=false
+
+# Backend build procedure
+cd $TRAVIS_BUILD_DIR/backend
+virtualenv venv -p python3
+source venv/bin/activate
+pip3 install -r requirements.txt
+
+cd $TRAVIS_BUILD_DIR
+python3 backend
