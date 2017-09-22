7aff67ab234e5ce079991a4c61fec94232419e88
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2017-09-22 10:47:45 +0200

    API: use new dbConnector

--- backend/liberouterapi/modules/users.py
+++ backend/liberouterapi/modules/users.py
@@ -13,41 +13,25 @@ user_db = dbConnector()
 
 users = Module('users', __name__, url_prefix='/users', no_version=True)
 
-def count_users():
-    return db.users.count()
-
-def lookup_user(user):
-    user_id = user.get("user_id", None)
-
-    query = {"$or" : [
-        {"_id" : None if user_id == None else ObjectId(user_id)},
-        {"username" : user.get("username", None)},
-        {"email" : user.get("email", None)}
-        ]}
-
-    cursor = db.users.find(query)
-
-    return list(cursor)
-
 def user_exists(user):
-    return False if db.users.find({"$or" : [
-        {"username" : user.get("username", None)},
-        {"email" : user.get("email", None)}
-        ]}).count() == 0 else True
+    if db.get("users", "username", user.username):
+        return True
+    elif db.get("users", "email", user.email):
+        return True
+    return False
 
 @auth.required()
 def get_users():
-    res = list(db.users.find())
+    res = list(db.getAll("users"))
 
     # Remove password hash from the resulting query
     for user in res:
         user.pop("password", None)
-        user["id"] = user.pop("_id", None)
     return(json_util.dumps(res))
 
 @auth.required()
 def get_user(user_id):
-    user = dict(db.get("users", "id", user_id))
+    user = db.get("users", "id", user_id)
     user.pop('password', None)
     return(json_util.dumps(user))
 
@@ -64,11 +48,7 @@ def unprotected_add_user(user):
     """
     user.password = auth.create_hash(user.password)
 
-    d_user = user.to_dict()
-
-    keys = list(d_user.keys())
-
-    res = user_db.insert("users", keys, [d_user[key] for key in keys])
+    res = user_db.insert("users", user.to_dict())
     return(res)
 
 
@@ -113,55 +93,47 @@ def edit_user(user_id):
     TODO: differentiate between PUT and PATCH -> PATCH partial update
     """
     user = User.from_dict(request.get_json())
-    user.user_id = user_id
-
-    print(request.get_json())
+    user.id = user_id
 
     session_id = request.headers.get('Authorization', None)
     session = auth.lookup(session_id)
 
     if session["user"].role != Role.admin:
         # We must check if the user is editing themselves
-        if user_id != session["user"].user_id:
+        if user_id != session["user"].id:
             raise UserException("Insufficient privileges. Non-admin users can edit only themselves",
                     status_code=401)
 
     # Create basic query for user updating
-    query = {
-            "$set" : {}
-            }
+    query = dict()
 
     # If the user updates their profile check for all fields to be updated
     if user.first_name and user.first_name != "":
-        query["$set"]["first_name"] = user.first_name
+        query["first_name"] = user.first_name
 
     if user.last_name and user.last_name != "":
-        query["$set"]["last_name"] = user.last_name
+        query["last_name"] = user.last_name
 
     if user.email and user.email != "":
-        query["$set"]["email"] = user.email
+        query["email"] = user.email
 
     if user.role != None and user.role >= 0:
-        query["$set"]["role"] = user.role
+        query["role"] = user.role
 
     if user.settings and user.settings != {}:
-        query['$set']['settings'] = user.settings
+        query['settings'] = user.settings
 
     # In case of password change, verify that it is really him (revalidate their password)
     if user.password and user.password != "":
         auth_verify = User.from_dict(auth.login(user))
 
-        query["$set"]["password"] = auth.create_hash(user_data["password_new"])
+        query["password"] = auth.create_hash(user_data["password_new"])
 
-    if len(query["$set"]) == 0:
+    if len(query.keys()) == 0:
         raise UserException("Nothing to update", status_code=400)
 
     # Update the user and return updated document
-    res = db.users.find_one_and_update(
-            {'_id' : ObjectId(user.user_id)},
-            query,
-            return_document=pymongo.ReturnDocument.AFTER)
-
+    res = db.update("users", "id", user.id, query)
     # Remove password hash from the response
     del res['password']
 
