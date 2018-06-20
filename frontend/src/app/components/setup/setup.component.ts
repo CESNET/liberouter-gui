8ba66555bcf10d816e2275698b9a0d2852541a30
Jakub Man <jakubman1@gmail.com>
2018-06-20 08:38:51 +0200

    Frontend CHANGE simplify color loading in login and setup

--- frontend/src/app/components/setup/setup.component.ts
+++ frontend/src/app/components/setup/setup.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'app/services';
-import { AppConfigService } from 'app/services/app-config.service';
 
 @Component({
   selector: 'app-setup',
@@ -10,22 +9,6 @@ import { AppConfigService } from 'app/services/app-config.service';
   providers : [AuthService]
 })
 export class SetupComponent implements OnInit {
-
-    // Default color theme is set to prevent errors before the actual config is loaded.
-    private config: object = {
-        "colorTheme": {
-            "colorMain": "#313747",
-            "colorHighlight": "#717787",
-            "colorSelected": "#575d6d",
-            "colorSelected2": "#b0b6c6",
-            "colorBackground": "#fafafa",
-            "colorText": "#111",
-            "colorTextInverse": "#fff",
-            "colorLink": "#111",
-            "colorLinkHover": "#333",
-            "colorHeading": "#555"
-        }};
-
     admin = {
         username : '',
         password : '',
@@ -34,12 +17,9 @@ export class SetupComponent implements OnInit {
 
     error = '';
 
-    constructor(private authService: AuthService, private router: Router, private appConfig: AppConfigService) { }
+    constructor(private authService: AuthService, private router: Router) { }
 
     ngOnInit() {
-        this.appConfig.get().subscribe(data => {
-            this.config = data;
-        });
     }
 
     onSubmit() {
